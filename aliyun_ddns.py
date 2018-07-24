349f788b5e88feec31cb06245e4c8b3844420b98
bertsir <bertsir@163.com>
2018-07-23 20:42:06 -0500

    Update aliyun_ddns.py

--- aliyun_ddns.py
+++ aliyun_ddns.py
@@ -10,6 +10,9 @@ import urllib
 import hashlib
 import hmac
 import time
+from urllib2 import urlopen
+from urllib2 import Request
+from urllib import urlencode
 
 from bs4 import BeautifulSoup as BS
 
@@ -90,13 +93,9 @@ def update_yun(ip):
 		print result
 
 def get_curr_ip():
-	headers = {
-		'content-type': 'text/html',
-		'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'
-	}
-	resp = requests.get('http://www.baidu.com/s?word=ip&_t={}'.format(int(time.time()), headers=headers))
-	soup = BS(resp.content, 'html.parser')
-	return soup.select('#1')[0]['fk']
+	opener = urlopen('https://wtfismyip.com/text')
+	strg = opener.read()
+	return strg
 
 def get_lastest_local_ip():
 	"""
